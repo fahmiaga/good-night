e27916548af6c1390ef255d0bad17274f37495c6
Fahmi Aga Aditya <fahmi.aga@gmail.com>
2025-09-05 10:27:03 +0700

    initial User model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_many :sleep_records, dependent: :destroy
+
+
+  has_many :active_follows, class_name: "Follow", foreign_key: :follower_id, dependent: :destroy
+  has_many :followings, through: :active_follows, source: :followed
+
+
+  has_many :passive_follows, class_name: "Follow", foreign_key: :followed_id, dependent: :destroy
+  has_many :followers, through: :passive_follows, source: :follower
+
+
+  validates :name, presence: true
+end
