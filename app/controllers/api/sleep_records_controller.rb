7fad4543fb4713ee580b565613a06996c397d71e
Fahmi Aga Aditya <fahmi.aga@gmail.com>
2025-09-05 11:56:34 +0700

    Implement Show all sleep record

--- app/controllers/api/sleep_records_controller.rb
+++ app/controllers/api/sleep_records_controller.rb
@@ -1,7 +1,18 @@
 class Api::SleepRecordsController < ApplicationController
+def index
+  user =  current_user
+  records = user.sleep_records.order(created_at: :desc).limit(100)
+  render json: records.as_json(
+    only: [ :id, :user_id, :start_time, :end_time, :duration_seconds, :created_at ],
+    include: {
+      user: { only: [ :id, :name ] }
+    }
+  )
+end
+
 def create
-  rec = current_user.sleep_records.create!(start_time: create_params[:start_time] || Time.current)
-  render json: rec.as_json(
+  record= current_user.sleep_records.create!(start_time: create_params[:start_time] || Time.current)
+  render json: record.as_json(
     only: [ :id, :user_id, :start_time, :end_time, :duration_seconds, :created_at ],
     include: {
       user: { only: [ :id, :name ] }
