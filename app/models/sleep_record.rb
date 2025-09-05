bda7ebf62ad628bf4f58a7795a4ee849837348de
Fahmi Aga Aditya <fahmi.aga@gmail.com>
2025-09-05 10:27:24 +0700

    initial Sleep Record  model

---
+++ app/models/sleep_record.rb
@@ -0,0 +1,28 @@
+class SleepRecord < ApplicationRecord
+  belongs_to :user
+
+  validates :start_time, presence: true
+  validate :end_after_start
+
+  before_save :compute_duration_seconds, if: -> { end_time_changed? && end_time.present? }
+
+  scope :finished, -> { where.not(end_time: nil) }
+
+
+  def duration_seconds
+    self[:duration_seconds] || (end_time && start_time && (end_time - start_time).to_i)
+  end
+
+  private
+
+  def end_after_start
+    return if end_time.blank? || start_time.blank?
+    errors.add(:end_time, "must be after start_time") if end_time <= start_time
+  end
+
+  def compute_duration_seconds
+    if end_time && start_time
+      self.duration_seconds = (end_time - start_time).to_i
+    end
+  end
+end
