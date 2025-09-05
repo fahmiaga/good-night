a46cc589ec4c27bdd84df9bb1fc3b27354629c0a
Fahmi Aga Aditya <fahmi.aga@gmail.com>
2025-09-05 13:49:36 +0700

    Implement Show sleep record by id

--- app/controllers/api/sleep_records_controller.rb
+++ app/controllers/api/sleep_records_controller.rb
@@ -1,27 +1,42 @@
 class Api::SleepRecordsController < ApplicationController
-def index
-  user =  current_user
-  records = user.sleep_records.order(created_at: :desc).limit(100)
-  render json: records.as_json(
-    only: [ :id, :user_id, :start_time, :end_time, :duration_seconds, :created_at ],
-    include: {
-      user: { only: [ :id, :name ] }
-    }
-  )
-end
+  before_action :set_sleep_record, only: [ :show ]
 
-def create
-  record= current_user.sleep_records.create!(start_time: create_params[:start_time] || Time.current)
-  render json: record.as_json(
-    only: [ :id, :user_id, :start_time, :end_time, :duration_seconds, :created_at ],
-    include: {
-      user: { only: [ :id, :name ] }
-    }
-  ), status: :created
-end
+  def index
+    user =  current_user
+    records = user.sleep_records.order(created_at: :desc).limit(100)
+    render json: records.as_json(
+      only: [ :id, :user_id, :start_time, :end_time, :duration_seconds, :created_at ],
+      include: {
+        user: { only: [ :id, :name ] }
+      }
+    )
+  end
+
+  def create
+    record= current_user.sleep_records.create!(start_time: create_params[:start_time] || Time.current)
+    render json: record.as_json(
+      only: [ :id, :user_id, :start_time, :end_time, :duration_seconds, :created_at ],
+      include: {
+        user: { only: [ :id, :name ] }
+      }
+    ), status: :created
+  end
+
+  def show
+    render json: @sleep_record.as_json(
+      only: [ :id, :user_id, :start_time, :end_time, :duration_seconds ],
+      include: {
+        user: { only: [ :id, :name ] }
+      }
+    )
+  end
 
 private
   def create_params
     params.permit(:start_time)
+  end
+
+  def set_sleep_record
+    @sleep_record = current_user.sleep_records.find(params[:id])
   end
 end
